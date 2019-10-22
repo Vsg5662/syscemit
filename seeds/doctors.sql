0eea48f18d298b21ba02c66af757eaf05cdb276e
Jonathan Oliveira <jonathan.oliveira24@fatecitapetininga.edu.br>
2019-10-22 20:49:10 -0300

    Adicionado novos arquivos para o banco
    -doctor.sql
    -zones.sql
    -ruasItapetininga.sql
    -Registries.sql
    -groves.sql

---
+++ seeds/doctors.sql
@@ -0,0 +1,9 @@
+INSERT INTO doctors (name, crm) VALUES
+('Dr. Angelo Scaldini Salomão','134.764'),
+('Dra Areana Diogo N. Mendonça','104176'),
+('Dra. Christiane Akemi Kojima','134.804'),
+('Dr. Fabio Henrique Mendonça','91.596'),
+('Dr. Fernando José Secanho','142.066'),
+('Dra. Marcelle Gattass Ayub Antunes','130.619'),
+('Dr. Murilo Gattass Ayub','108.323'),
+('Dr. Rafael Miranda Silvestre','125.381');
---
+++ seeds/zones.sql
@@ -0,0 +1,11 @@
+INSERT INTO zones (description, complement) VALUES
+('Ala','1'),
+('Ala','2'),
+('Cemitério do Santissimo Sacramento','1'),
+('Cemitério do Santissimo Sacramento','2'),
+('Cruzeiro',''),
+('Quadra','A'),
+('Quadra','B'),
+('Quadra','C'),
+('Quadra','D'),
+('São joao Batista','');
